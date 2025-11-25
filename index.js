51d5a9afdfa79aa9e0327a6e9b99fdf80461eece
masrafi143 <masrafiofficial143@gmail.com>
2025-11-25 10:58:00 +0600

    delete-events api

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ async function run() {
     //   res.send(result);
     // });
 
-    // delete-events
+    // delete-events 
     app.delete("/events/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
